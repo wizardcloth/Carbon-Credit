e5c19314eea96890e904f9c3a7f8f4047c2fb17e
wizardcloth <ujjwal.chauhan8130@gmail.com>
2025-09-30 19:39:14 +0530

    setup done

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button'
+function App() {
+
+  return (
+    <>
+      <h1>hello</h1>
+      <Button className='bg-red-500'>hello</Button>
+    </>
+  )
+}
+
+export default App
